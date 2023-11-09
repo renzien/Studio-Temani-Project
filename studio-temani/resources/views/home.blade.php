2afaf188b07036a00e7003e583f125a9f4b24a8c
renzien <alifrizki1011@gmail.com>
2023-11-09 11:13:11 +0700

    rework navbar

--- studio-temani/resources/views/home.blade.php
+++ studio-temani/resources/views/home.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.main')
 @section('container')
-    <header class="my-2">
-        <div class="w-screen">
-            <img src="assets/img/bg-hero.png" alt="Studio Temani">
+    <header>
+        <div class="h-screen">
+            <img src="assets/img/bg-hero.png" alt="Studio Temani" class="absolute">
+            <div class="flex items-center ml-44 h-screen relative text-white">
+                <p class="font-quicksands font-normal text-2xl leading-normal">Summer 2023</p>
+            </div>
         </div>
     </header>
 @endsection
--- studio-temani/resources/views/partials/navbar.blade.php
+++ studio-temani/resources/views/partials/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav class="bg-white p-3">
+<nav class="bg-white p-5">
     <div class="flex items-center justify-between">
-        <div class="image w-36">
+        <div class="image w-36 mr-10">
             <img src="assets/img/logo.png" alt="Studio Temani">
         </div>
 
-        <div class="flex mt-3 items-center gap-2 space-x-4 font-quicksand">
+        <div class="flex mt-2 items-center gap-2 space-x-4 font-quicksand">
             <a href="#" class="text-black font-bold hover:text-gray-600">Home</a>
             <a href="#" class="text-black font-bold hover:text-gray-600">About</a>
             <a href="#" class="text-black font-bold hover:text-gray-600">Studio</a>
@@ -12,7 +12,7 @@
             <a href="#" class="text-black font-bold hover:text-gray-600">Contact</a>
         </div>
 
-        <div class="booking mt-3">
+        <div class="booking mt-2 mr-3">
             <button class="bg-black hover:bg-gray-700 text-white font-quicksands font-bold py-2 px-4 rounded-lg">Booking Now</button>
         </div>
     </div>
