28fb8abc82b5033f4f0217fa0013d00512e14d51
renzien <alifrizki1011@gmail.com>
2023-11-29 12:16:11 +0700

    home posting up

--- studio-temani/resources/views/admin/adminHome.blade.php
+++ studio-temani/resources/views/admin/adminHome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,22 +9,30 @@
 
     {{-- Favicon --}}
     <link rel="shortcut icon" href="assets/img/svg/favicon.svg" type="image/x-icon">
-    
+
     {{-- Remixicon --}}
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
 
     {{-- CSS --}}
     <link rel="stylesheet" href="assets/css/app.css">
     <link rel="stylesheet" href="assets/css/app-dark.css">
+
+    {{-- Filepond --}}
+    <link rel="stylesheet" href="assets/css/filepond/filepond.css">
+    <link rel="stylesheet" href="assets/css/filepond-plugin-image-preview/filepond-plugin-image-preview.css">
+
+    {{-- Toastify --}}
+    <link rel="stylesheet" href="assets/js/toastify-js/src/toastify.css">
 </head>
+
 <body>
     <script src="assets/js/initTheme.js"></script>
     @include('admin.partials.sidebar')
     <div id="main">
         <header class="mb-3">
-          <a href="#" class="burger-btn d-block d-xl-none">
-            <i class="ri-menu-line"></i>
-          </a>
+            <a href="#" class="burger-btn d-block d-xl-none">
+                <i class="ri-menu-line"></i>
+            </a>
         </header>
         @yield('homeeditor')
     </div>
@@ -35,3 +44,16 @@
 {{-- Tiny MCE --}}
 <script src="assets/js/tinymce/tinymce.min.js"></script>
 <script src="assets/js/tinymce/static/pages/tinymce.js"></script>
+{{-- Filepond --}}
+<script src="assets/css/filepond-plugin-file-validate-size/filepond-plugin-file-validate-size.min.js"></script>
+<script src="assets/css/filepond-plugin-file-validate-type/filepond-plugin-file-validate-type.min.js"></script>
+<script src="assets/css/filepond-plugin-image-crop/filepond-plugin-image-crop.min.js"></script>
+<script src="assets/css/filepond-plugin-image-exif-orientation/filepond-plugin-image-exif-orientation.min.js"></script>
+<script src="assets/css/filepond-plugin-image-filter/filepond-plugin-image-filter.min.js"></script>
+<script src="assets/css/filepond-plugin-image-preview/filepond-plugin-image-preview.min.js"></script>
+<script src="assets/css/filepond-plugin-image-resize/filepond-plugin-image-resize.min.js"></script>
+<script src="assets/css/filepond/filepond.js"></script>
+<script src="assets/js/filepond.js"></script>
+
+{{-- Toastify --}}
+<script src="assets/js/toastify-js/src/toastify.js"></script>
--- studio-temani/resources/views/admin/layouts/adHome.blade.php
+++ studio-temani/resources/views/admin/layouts/adHome.blade.php
@@ -7,7 +7,7 @@
                 <div class="col-12 col-md-6 order-md-1 order-last">
                     <h3>Edit Home</h3>
                     <p class="text-subtitle text-muted">
-                        Gunakan posting editor ini untuk melakukan edit 
+                        Gunakan posting editor ini untuk melakukan edit
                         pada halaman Home.
                     </p>
                 </div>
@@ -34,6 +34,65 @@
                         </div>
                         <div class="card-body">
                             <textarea name="" id="default" cols="30" rows="10"></textarea>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Update Hero Background</h5>
+                        </div>
+                        <div class="card-content">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Masukkan foto dengan ukuran 1920x1080. Agar tampilan lebih baik,
+                                    gunakan foto dengan format .png
+                                </p>
+                                <!-- File uploader with image preview -->
+                                <input type="file" class="image-preview-filepond" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="col-12 col-md-6 order-md-1 order-last">
+            <h3>Edit About</h3>
+            <p class="text-subtitle text-muted">
+                Gunakan posting editor ini untuk melakukan
+                edit pada halaman About yang telah disediakan.
+            </p>
+        </div>
+        <section class="section">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Edit Judul About</h4>
+                        </div>
+                        <div class="card-body">
+                            <textarea name="" id="default" cols="30" rows="10"></textarea>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Edit Isi About</h4>
+                        </div>
+                        <div class="card-body">
+                            <textarea name="" id="default" cols="30" rows="10"></textarea>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Update Hero Background</h5>
+                        </div>
+                        <div class="card-content">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Masukkan foto dengan ukuran 1920x1080. Agar tampilan lebih baik,
+                                    gunakan foto dengan format .png
+                                </p>
+                                <!-- File uploader with image preview -->
+                                <input type="file" class="image-preview-filepond" />
+                            </div>
                         </div>
                     </div>
                 </div>
